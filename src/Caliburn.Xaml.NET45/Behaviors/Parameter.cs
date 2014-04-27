e4daa11279b1dc2c8d2d7b068a4feadb3a04bef1
Thomas Ibel <tibel@users.noreply.github.com>
2014-04-27 15:18:45 +0200

    add Parameter class

---
+++ src/Caliburn.Xaml.NET45/Behaviors/Parameter.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace Caliburn.Xaml
+{
+    /// <summary>
+    /// Represents a parameter of a <see cref="System.Windows.Interactivity.TriggerAction"/>.
+    /// </summary>
+    public class Parameter : DependencyObject, IAttachedObject
+    {
+        /// <summary>
+        /// Identifies the <seealso cref="Parameter.Value"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof (object),
+            typeof (Parameter), new PropertyMetadata(null, OnValueChanged));
+
+        private DependencyObject _associatedObject;
+        private WeakReference _owner;
+
+        /// <summary>
+        /// Gets or sets the value of the parameter.
+        /// </summary>
+        [Category("Common Properties")]
+        public object Value
+        {
+            get { return GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        void IAttachedObject.Attach(DependencyObject dependencyObject)
+        {
+            _associatedObject = dependencyObject;
+        }
+
+        void IAttachedObject.Detach()
+        {
+            _associatedObject = null;
+        }
+
+        DependencyObject IAttachedObject.AssociatedObject
+        {
+            get { return _associatedObject; }
+        }
+
+        /// <summary>
+        /// Gets the owner of this instance.
+        /// </summary>
+        protected IHaveParameters Owner
+        {
+            get { return _owner == null ? null : _owner.Target as IHaveParameters; }
+        }
+
+        /// <summary>
+        /// Makes the parameter aware of the <see cref="IHaveParameters"/> that it is attached to.
+        /// </summary>
+        /// <param name="owner">The owner of the parameter.</param>
+        public void MakeAwareOf(IHaveParameters owner)
+        {
+            _owner = (owner != null) ? new WeakReference(owner) : null;
+        }
+
+        private static void OnValueChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var owner = ((Parameter)d).Owner;
+            if (owner != null)
+                owner.UpdateAvailability();
+        }
+    }
+}
---
+++ src/Caliburn.Xaml.WIN81/Behaviors/IHaveParameters.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Caliburn.Xaml
+{
+    /// <summary>
+    /// Indicates that a <see cref="TriggerAction"/> is parametrized.
+    /// </summary>
+    public interface IHaveParameters
+    {
+        /// <summary>
+        /// Represents the parameters of the action.
+        /// </summary>
+        AttachedCollection<Parameter> Parameters { get; }
+
+        /// <summary>
+        /// Forces an update of the IsEnabled state.
+        /// </summary>
+        void UpdateAvailability();
+    }
+}
