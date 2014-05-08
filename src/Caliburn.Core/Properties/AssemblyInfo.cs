80b94cdf012626ef88653d889ca8ef8ccd94e2a1
Thomas Ibel <tibel@users.noreply.github.com>
2014-05-08 07:59:26 +0200

    update assembly info

--- src/Caliburn.Core/Properties/AssemblyInfo.cs
+++ src/Caliburn.Core/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Caliburn.Light")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Caliburn.Core")]
+[assembly: AssemblyDescription("The portable core of Caliburn.Light")]
--- src/Caliburn.Xaml.NET45/Properties/AssemblyInfo.cs
+++ src/Caliburn.Xaml.NET45/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Caliburn.Xaml")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Caliburn.Xaml.NET45")]
+[assembly: AssemblyDescription("The XAML aware parts of Caliburn.Light")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
