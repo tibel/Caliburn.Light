45bc7b65ae532cbcc354ab7fec5c2bf1fe0de953
Thomas Ibel <tibel@users.noreply.github.com>
2014-05-08 20:04:26 +0200

    fix publish script

--- nuget/publish-package.cmd
+++ nuget/publish-package.cmd
@@ -1,3 +1,4 @@
 @ECHO OFF
-..\packages\NuGet.CommandLine.2.8.1\tools\NuGet.exe push Caliburn.Light.*.nupkg
+for /f "delims=" %%F in ('dir *.nupkg /b /o-n') do set PACKAGE=%%F
+..\packages\NuGet.CommandLine.2.8.1\tools\NuGet.exe push %PACKAGE%
 pause
