226c8bcf2d1a6446bfca161df4dd8a843dcbf4b3
Thomas Ibel <tibel@users.noreply.github.com>
2014-10-18 12:23:20 +0200

    add ExecutionContextResolver

---
+++ src/Caliburn.Xaml.NET45/Behaviors/ExecutionContextResolver.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows;
+
+namespace Caliburn.Light
+{
+    /// <summary>
+    /// Special value to resolve <see cref="CoroutineExecutionContext"/> parameter.
+    /// </summary>
+    public sealed class ExecutionContextResolver : Freezable, ISpecialValue
+    {
+        /// <summary>
+        /// Identifies the <seealso cref="ExecutionContextResolver.SourceOverride"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty SourceOverrideProperty = DependencyProperty.Register(
+            "SourceOverride", typeof (object), typeof (ExecutionContextResolver), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets or sets the override for <see cref="CoroutineExecutionContext.Source"/>.
+        /// </summary>
+        public object SourceOverride
+        {
+            get { return GetValue(SourceOverrideProperty); }
+            set { SetValue(SourceOverrideProperty, value); }
+        }
+
+        /// <summary>
+        /// Creates a new instance.
+        /// </summary>
+        /// <returns>The new instance.</returns>
+        protected override Freezable CreateInstanceCore()
+        {
+            return new ExecutionContextResolver();
+        }
+
+        /// <summary>
+        /// Resolves the value of this instance.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns>The resolved value.</returns>
+        public object Resolve(CoroutineExecutionContext context)
+        {
+            var sourceOverride = SourceOverride;
+            if (sourceOverride != null)
+                context.Source = sourceOverride;
+
+            return context;
+        }
+    }
+}
---
+++ src/Caliburn.Xaml.WP8/Behaviors/ExecutionContextResolver.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace Caliburn.Light
+{
+    /// <summary>
+    /// Special value to resolve <see cref="CoroutineExecutionContext"/> parameter.
+    /// </summary>
+    public sealed class ExecutionContextResolver : DependencyObject, ISpecialValue
+    {
+        /// <summary>
+        /// Identifies the <seealso cref="ExecutionContextResolver.SourceOverride"/> dependency property.
+        /// </summary>
+        public static readonly DependencyProperty SourceOverrideProperty = DependencyProperty.Register(
+            "SourceOverride", typeof (object), typeof (ExecutionContextResolver), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets or sets the override for <see cref="CoroutineExecutionContext.Source"/>.
+        /// </summary>
+        public object SourceOverride
+        {
+            get { return GetValue(SourceOverrideProperty); }
+            set { SetValue(SourceOverrideProperty, value); }
+        }
+
+        /// <summary>
+        /// Resolves the value of this instance.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <returns>The resolved value.</returns>
+        public object Resolve(CoroutineExecutionContext context)
+        {
+            var sourceOverride = SourceOverride;
+            if (sourceOverride != null)
+                context.Source = sourceOverride;
+
+            return context;
+        }
+    }
+}
