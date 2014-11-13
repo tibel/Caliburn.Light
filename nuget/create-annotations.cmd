c81819d02d404feb75a5c0d52d2badb21b153b1e
Thomas Ibel <tibel@users.noreply.github.com>
2014-11-13 08:45:24 +0100

    fix nuget path

--- nuget/create-annotations.cmd
+++ nuget/create-annotations.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
 del *.nupkg
-..\packages\NuGet.CommandLine.2.8.2\tools\NuGet.exe pack Caliburn.Light.Annotations.nuspec
+..\packages\NuGet.CommandLine.2.8.3\tools\NuGet.exe pack Caliburn.Light.Annotations.nuspec
 pause
--- nuget/publish-package.cmd
+++ nuget/publish-package.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
 for /f "delims=" %%F in ('dir *.nupkg /b /o-n') do set PACKAGE=%%F
-..\packages\NuGet.CommandLine.2.8.2\tools\NuGet.exe push %PACKAGE%
+..\packages\NuGet.CommandLine.2.8.3\tools\NuGet.exe push %PACKAGE%
 pause
