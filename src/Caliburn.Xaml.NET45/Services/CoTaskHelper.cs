480cf65f62882dd0d1a7df1ea907148138307514
Thomas Ibel <tibel@users.noreply.github.com>
2015-03-21 14:12:17 +0100

    [WPF] better fallback for active window

--- src/Caliburn.Xaml.NET45/Services/CoTaskHelper.cs
+++ src/Caliburn.Xaml.NET45/Services/CoTaskHelper.cs
@@ -7,20 +7,21 @@ namespace Caliburn.Light
     {
         public static Window GetActiveWindow(CoroutineExecutionContext context)
         {
+            return GetWindowFromContext(context) ?? GetFirstActiveWindow();
+        }
+
+        private static Window GetWindowFromContext(CoroutineExecutionContext context)
+        {
             var view = context.Source as DependencyObject;
-            if (view != null)
-            {
-                return Window.GetWindow(view);
-            }
+            return view != null ? Window.GetWindow(view) : null;
+        }
 
+        private static Window GetFirstActiveWindow()
+        {
             var application = Application.Current;
-            if (application != null)
-            {
-                var active = application.Windows.OfType<Window>().FirstOrDefault(x => x.IsActive);
-                return active ?? application.MainWindow;
-            }
-
-            return null;
+            if (application == null) return null;
+            var active = application.Windows.OfType<Window>().FirstOrDefault(x => x.IsActive);
+            return active ?? application.MainWindow;
         }
     }
 }
