b378ca7b052353c508809512b0f1111a4d8bdaec
Thomas Ibel <tibel@users.noreply.github.com>
2015-04-02 09:39:34 +0200

    change version to 1.7

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Resources;
 [assembly: CLSCompliant(true)]
 #endif
 
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("1.7.0.0")]
+[assembly: AssemblyFileVersion("1.7.0.0")]
