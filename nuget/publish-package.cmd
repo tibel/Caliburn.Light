d94a3bb1b596b2afa0cc86adcbb67ae8c4a4f13c
Thomas Ibel <tibel@users.noreply.github.com>
2015-12-14 21:09:06 +0100

    update nuget 3.3.0

--- nuget/publish-package.cmd
+++ nuget/publish-package.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
 for /f "delims=" %%F in ('dir *.nupkg /b /o-n') do set PACKAGE=%%F
-..\packages\NuGet.CommandLine.2.8.6\tools\NuGet.exe push %PACKAGE%
+..\packages\NuGet.CommandLine.3.3.0\tools\NuGet.exe push %PACKAGE%
 pause
