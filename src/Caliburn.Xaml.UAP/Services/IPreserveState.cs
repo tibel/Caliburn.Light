4d3dd92ab6d9136c997e38069f49eb09dc7c4109
Thomas Ibel <tibel@users.noreply.github.com>
2016-06-04 17:44:25 +0200

    add new interfaces for view models

---
+++ src/Caliburn.Xaml.UAP/Services/IPreserveState.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Caliburn.Light
+{
+    /// <summary>
+    /// Denotes a class which wants to preserve state accross session lifetime.
+    /// </summary>
+    public interface IPreserveState
+    {
+        /// <summary>
+        /// Restores previously saved state.
+        /// </summary>
+        /// <param name="state">A collection of state items to restore.</param>
+        void RestoreState(IReadOnlyDictionary<string, object> state);
+
+        /// <summary>
+        /// Saves the current state.
+        /// </summary>
+        /// <param name="state">A collection where to put state items.</param>
+        void SaveState(IDictionary<string, object> state);
+    }
+}
