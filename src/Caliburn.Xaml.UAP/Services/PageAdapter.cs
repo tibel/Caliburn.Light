81e4cca4e186f7298bec3cf38ed88e41105a787a
Thomas Ibel <tibel@users.noreply.github.com>
2016-06-05 14:04:42 +0200

    add SuspensionPageAdapter

--- src/Caliburn.Xaml.UAP/Services/PageAdapter.cs
+++ src/Caliburn.Xaml.UAP/Services/PageAdapter.cs
@@ -1,40 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
 namespace Caliburn.Light
 {
+    /// <summary>
+    /// Integrate framework lifetime handling into <see cref="Page"/> navigation events.
+    /// </summary>
     public class PageAdapter : IPageAdapter
     {
-        private static DependencyProperty PageKeyProperty =
-            DependencyProperty.RegisterAttached("_PageKey", typeof(string), typeof(PageAdapter), null);
-
         private readonly IViewModelLocator _viewModelLocator;
         private readonly IViewModelBinder _viewModelBinder;
-        private readonly ISuspensionManager _suspensionManager;
 
         /// <summary>
         /// Creates an instance of <see cref="PageAdapter" />.
         /// </summary>
         /// <param name="viewModelLocator">The view-model locator.</param>
         /// <param name="viewModelBinder">The view-model binder.</param>
-        /// <param name="suspensionManager">The suspension manager.</param>
-        public PageAdapter(IViewModelLocator viewModelLocator, IViewModelBinder viewModelBinder, ISuspensionManager suspensionManager)
+        public PageAdapter(IViewModelLocator viewModelLocator, IViewModelBinder viewModelBinder)
         {
             if (viewModelLocator == null)
                 throw new ArgumentNullException(nameof(viewModelLocator));
             if (viewModelBinder == null)
                 throw new ArgumentNullException(nameof(viewModelBinder));
-            if (suspensionManager == null)
-                throw new ArgumentNullException(nameof(suspensionManager));
 
             _viewModelLocator = viewModelLocator;
             _viewModelBinder = viewModelBinder;
-            _suspensionManager = suspensionManager;
         }
 
+        /// <summary>
+        /// Invoked immediately before the Page is unloaded and is no longer the current source of a parent Frame.
+        /// </summary>
+        /// <param name="page">The page.</param>
+        /// <param name="e">The event data.</param>
         public void OnNavigatingFrom(Page page, NavigatingCancelEventArgs e)
         {
             if (e.Cancel) return;
@@ -54,15 +52,14 @@ namespace Caliburn.Light
             }
         }
 
+        /// <summary>
+        /// Invoked immediately after the Page is unloaded and is no longer the current source of a parent Frame.
+        /// </summary>
+        /// <param name="page">The page.</param>
+        /// <param name="e">The event data.</param>
         public void OnNavigatedFrom(Page page, NavigationEventArgs e)
         {
-            var navigationAware = page.DataContext as INavigationAware;
-            if (navigationAware != null)
-            {
-                navigationAware.OnNavigatedFrom();
-            }
-
-            SaveState(page);
+            OnNavigatedFromCore(page, e);
 
             var deactivator = page.DataContext as IDeactivate;
             if (deactivator != null)
@@ -72,16 +69,34 @@ namespace Caliburn.Light
             }
         }
 
-        public void OnNavigatedTo(Page page, NavigationEventArgs e)
+        /// <summary>
+        /// Calls <see cref="INavigationAware.OnNavigatedFrom" /> on the view model
+        /// </summary>
+        /// <param name="page">The page.</param>
+        /// <param name="e">The event data.</param>
+        protected virtual void OnNavigatedFromCore(Page page, NavigationEventArgs e)
         {
-            EnsureViewModel(page);
-            RestoreState(page, e.NavigationMode);
-
             var navigationAware = page.DataContext as INavigationAware;
             if (navigationAware != null)
             {
-                navigationAware.OnNavigatedTo(e.Parameter);
+                navigationAware.OnNavigatedFrom();
             }
+        }
+
+        /// <summary>
+        /// Invoked when the Page is loaded and becomes the current source of a parent Frame.
+        /// </summary>
+        /// <param name="page">The page.</param>
+        /// <param name="e">The event data.</param>
+        public void OnNavigatedTo(Page page, NavigationEventArgs e)
+        {
+            if (page.DataContext == null)
+            {
+                var viewModel = _viewModelLocator.LocateForView(page);
+                _viewModelBinder.Bind(viewModel, page, null);
+            }
+
+            OnNavigatedToCore(page, e);
 
             var activator = page.DataContext as IActivate;
             if (activator != null)
@@ -91,67 +106,16 @@ namespace Caliburn.Light
         }
 
         /// <summary>
-        /// Ensures that the DataContext is set.
+        /// Calls <see cref="INavigationAware.OnNavigatedTo(object)" /> on the view model.
         /// </summary>
         /// <param name="page">The page.</param>
-        protected void EnsureViewModel(Page page)
+        /// <param name="e">The event data.</param>
+        protected virtual void OnNavigatedToCore(Page page, NavigationEventArgs e)
         {
-            if (page.DataContext == null)
+            var navigationAware = page.DataContext as INavigationAware;
+            if (navigationAware != null)
             {
-                var viewModel = _viewModelLocator.LocateForView(page);
-                _viewModelBinder.Bind(viewModel, page, null);
-            }
-        }
-
-        /// <summary>
-        /// Saves the current state.
-        /// </summary>
-        /// <param name="page">The page.</param>
-        protected void SaveState(Page page)
-        {
-            var preserveState = page.DataContext as IPreserveState;
-            if (preserveState == null) return;
-
-            var pageKey = (string)page.GetValue(PageKeyProperty);
-            var frameState = _suspensionManager.SessionStateForFrame(page.Frame);
-            var pageState = new Dictionary<string, object>();
-            preserveState.SaveState(pageState);
-            frameState[pageKey] = pageState;
-        }
-
-        /// <summary>
-        /// Restore previously saved state.
-        /// </summary>
-        /// <param name="page">The page.</param>
-        /// <param name="navigationMode">The navigation mode.</param>
-        protected void RestoreState(Page page, NavigationMode navigationMode)
-        {
-            var frame = page.Frame;
-            var frameState = _suspensionManager.SessionStateForFrame(frame);
-            var pageKey = "Page-" + frame.BackStackDepth;
-            page.SetValue(PageKeyProperty, pageKey);
-
-            if (navigationMode == NavigationMode.New)
-            {
-                // Clear existing state for forward navigation when adding a new page to the navigation stack
-                var nextPageKey = pageKey;
-                int nextPageIndex = frame.BackStackDepth;
-                while (frameState.Remove(nextPageKey))
-                {
-                    nextPageIndex++;
-                    nextPageKey = "Page-" + nextPageIndex;
-                }
-            }
-            else
-            {
-                // Pass the preserved page state to the page, 
-                // using the same strategy for loading suspended state and recreating pages discarded from cache
-                var preserveState = page.DataContext as IPreserveState;
-                if (preserveState != null)
-                {
-                    var pageState = (Dictionary<string, object>)frameState[pageKey];
-                    preserveState.RestoreState(pageState);
-                }
+                navigationAware.OnNavigatedTo(e.Parameter);
             }
         }
     }
