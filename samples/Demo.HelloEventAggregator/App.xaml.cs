8bc7305570ba3d97cc7b62c542364688ca29fb63
Thomas Ibel <tibel@users.noreply.github.com>
2016-06-05 14:16:32 +0200

    get UAP demos working again

--- samples/Demo.HelloEventAggregator/App.xaml.cs
+++ samples/Demo.HelloEventAggregator/App.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using Caliburn.Light;
 using System;
 using System.Reflection;
-using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
@@ -31,8 +30,6 @@ namespace Demo.HelloEventAggregator
             _container = new SimpleContainer();
             IoC.Initialize(_container);
 
-            _container.RegisterSingleton<INavigationService, FrameAdapter>();
-            _container.RegisterSingleton<ISuspensionManager, SuspensionManager>();
             _container.RegisterSingleton<IPageAdapter, PageAdapter>();
             _container.RegisterSingleton<IEventAggregator, EventAggregator>();
             _container.RegisterSingleton<IViewModelLocator, ViewModelLocator>();
@@ -52,7 +49,7 @@ namespace Demo.HelloEventAggregator
         /// will be used such as when the application is launched to open a specific file.
         /// </summary>
         /// <param name="e">Details about the launch request and process.</param>
-        protected override async void OnLaunched(LaunchActivatedEventArgs e)
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
 #if DEBUG
             // Show graphics profiling information while debugging.
@@ -72,44 +69,23 @@ namespace Demo.HelloEventAggregator
             // just ensure that the window is active
             if (rootFrame == null)
             {
-                // Create a Frame to act as the navigation context and
-                // the navigation service to handle view model resolution and activation
+                // Create a Frame to act as the navigation context
                 rootFrame = new Frame();
-                _container.RegisterInstance(rootFrame);
-
-                //Associate the frame with a SuspensionManager key
-                var suspensionManager = _container.GetInstance<ISuspensionManager>();
-                suspensionManager.RegisterFrame(rootFrame, "AppFrame");
 
                 // Set the default language
                 rootFrame.Language = Windows.Globalization.ApplicationLanguages.Languages[0];
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    // Restore the saved session state only when appropriate
-                    try
-                    {
-                        await suspensionManager.RestoreAsync();
-                    }
-                    catch (SuspensionManagerException)
-                    {
-                        //Something went wrong restoring state.
-                        //Assume there is no state and continue
-                    }
-                }
-
                 // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
             }
 
-            var navigationService = _container.GetInstance<INavigationService>();
-            if (navigationService.Content == null)
+            if (rootFrame.Content == null)
             {
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation parameter
-                navigationService.Navigate<MainPage>(e.Arguments);
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
             }
 
             // Ensure the current window is active
@@ -124,22 +100,6 @@ namespace Demo.HelloEventAggregator
         private void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
-        }
-
-        /// <summary>
-        /// Invoked when application execution is being suspended.  Application state is saved
-        /// without knowing whether the application will be terminated or resumed with the contents
-        /// of memory still intact.
-        /// </summary>
-        /// <param name="e">Details about the suspend request.</param>
-        protected override async void OnSuspending(SuspendingEventArgs e)
-        {
-            var deferral = e.SuspendingOperation.GetDeferral();
-
-            var suspensionManager = _container.GetInstance<ISuspensionManager>();
-            await suspensionManager.SaveAsync();
-
-            deferral.Complete();
         }
     }
 }
--- samples/Demo.HelloSpecialValues/MainPage.xaml.cs
+++ samples/Demo.HelloSpecialValues/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using Caliburn.Light;
+using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
@@ -20,6 +21,21 @@ namespace Demo.HelloSpecialValues
         public MainPageViewModel Model
         {
             get { return DataContext as MainPageViewModel; }
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            IoC.GetInstance<IPageAdapter>().OnNavigatingFrom(this, e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            IoC.GetInstance<IPageAdapter>().OnNavigatedFrom(this, e);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            IoC.GetInstance<IPageAdapter>().OnNavigatedTo(this, e);
         }
     }
 }
