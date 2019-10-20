352e84b4df7a44f0fc3ae00206ef8b64f8fbcd32
Thomas Ibel <tibel@users.noreply.github.com>
2019-10-20 17:59:16 +0200

    Use SourceLink

--- nuget/create-package.cmd
+++
@@ -1,4 +0,0 @@
-@ECHO OFF & SETLOCAL
-del *.nupkg
-nuget.cmd pack Caliburn.Light.nuspec -Symbols
-pause
--- nuget/publish-package.cmd
+++
@@ -1,3 +0,0 @@
-@ECHO OFF & SETLOCAL
-for /f "delims=" %%F in ('dir *.nupkg /b /o-n') do nuget.cmd push %%F
-pause
