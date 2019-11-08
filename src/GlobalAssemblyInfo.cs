18c7222459ca5a22e079e77f6e1e9b7310119ac0
Thomas Ibel <tibel@users.noreply.github.com>
2019-11-08 17:04:33 +0100

    pump version

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Resources;
 [assembly: CLSCompliant(true)]
 #endif
 
-[assembly: AssemblyVersion("3.3.0.0")]
-[assembly: AssemblyFileVersion("3.3.0.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
