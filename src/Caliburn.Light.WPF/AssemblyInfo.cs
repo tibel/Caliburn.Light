b7db019922228dcb20e8c261702f6c84067db77e
Thomas Ibel <tibel@users.noreply.github.com>
2020-04-13 15:06:16 +0200

    remove GlobalAssemblyInfo

---
+++ src/Caliburn.Light.WPF/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+[assembly: XmlnsDefinition("https://github.com/tibel/Caliburn.Light/", "Caliburn.Light.WPF")]
+[assembly: XmlnsPrefix("https://github.com/tibel/Caliburn.Light/", "cal")]
