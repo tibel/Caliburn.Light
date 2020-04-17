4bd203586ea66add85b3b7b1139e39839629cdce
Thomas Ibel <tibel@users.noreply.github.com>
2020-04-17 10:31:59 +0200

    wpf designer quirks

--- src/Caliburn.Light.WPF/AssemblyInfo.cs
+++ src/Caliburn.Light.WPF/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Windows.Markup;
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
 
-[assembly: XmlnsDefinition("https://github.com/tibel/Caliburn.Light/", "Caliburn.Light.WPF")]
+[assembly: XmlnsDefinition("https://github.com/tibel/Caliburn.Light/", "Caliburn.Light.WPF", AssemblyName = "Caliburn.Light.WPF")]
 [assembly: XmlnsPrefix("https://github.com/tibel/Caliburn.Light/", "cal")]
